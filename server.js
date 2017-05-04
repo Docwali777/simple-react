a457f48876095d990fe8871d29b23d827793c2a5
docwali777 <drgauvin@mgmcare.org>
2017-05-04 13:12:32 -0400

    just intime

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 const path = require('path');
 
-app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname + '/src/'))
-})
+app.use(express.static(__dirname +  '/build'));
 
 app.listen(PORT, ()=>{
   console.log(`Server operating on PORT: ${PORT}`);
