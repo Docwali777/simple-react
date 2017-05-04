b1ce259b339e24b185052772a2b013ff783e7387
docwali777 <drgauvin@mgmcare.org>
2017-05-04 12:12:11 -0400

    2

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
+const path = require('path');
 
-
-app.use(express.static(__dirname + 'public'))
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname + '/build/index.html'))
+})
 
 app.listen(PORT, ()=>{
   console.log(`Server operating on PORT: ${PORT}`);
