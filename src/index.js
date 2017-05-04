2e8c9ba0fd5b17f03bf7f1ff3a6da75a8ac8b05b
docwali777 <drgauvin@mgmcare.org>
2017-05-04 15:53:04 -0400

    3

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ class App extends Component {
   render(){
     return(
       <div>
-    react-App-on Heroku
+    Let' Get this Party Started
+   <img src='https://udemy-images.udemy.com/course/480x270/1037666_91c9_2.jpg' />
       </div>
     )
   }
